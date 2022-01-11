9230c70db97c403cb5c795a0e2a95b63a08e4dbf
Sebastian-Lehrig1 <Sebastian.Lehrig1@ibm.com>
2022-01-11 23:24:05 +0100

    first build script to test

---
+++ kubeflow-access-management-ppc64le/build.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+cd components/access-management
+
+sed -i 's/GOARCH=amd64/GOARCH=ppc64le/g' Dockerfile
+
+sudo make build IMG=quay.io/ibm/${IMAGE} TAG=${RELEASE}
+
+
+set +x
+echo $quay_p | sudo docker login --username $quay_u --password-stdin https://quay.io
+set -x
+
+sudo docker push quay.io/ibm/${IMAGE}:${RELEASE}
