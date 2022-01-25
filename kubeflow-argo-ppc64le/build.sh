79c175dbcf538454088bacd460e7af68aea22fc3
Dr. Sebastian Lehrig <github@lehrig.de>
2022-01-25 23:41:42 +0100

    Update build.sh

--- kubeflow-argo-ppc64le/build.sh
+++ kubeflow-argo-ppc64le/build.sh
@@ -1,4 +1,19 @@
 #!/bin/sh
+
+#PreReq 1: docker (v19.03 as --progress wasn't supported by older versions)
+sudo apt-get remove docker docker-engine docker-ce docker.io -y
+sudo apt-get update && sudo apt-get install containerd python2.7 -y
+sudo ln -s /usr/bin/python2.7 /usr/bin/python2
+
+wget https://oplab9.parqtec.unicamp.br/pub/ppc64el/docker/version-19.03.8/ubuntu-bionic/docker-ce_19.03.8~3-0~ubuntu-bionic_ppc64el.deb
+wget https://oplab9.parqtec.unicamp.br/pub/ppc64el/docker/version-19.03.8/ubuntu-bionic/docker-ce-cli_19.03.8~3-0~ubuntu-bionic_ppc64el.deb
+sudo dpkg -i docker-ce-cli_19.03.8~3-0~ubuntu-bionic_ppc64el.deb docker-ce_19.03.8~3-0~ubuntu-bionic_ppc64el.deb
+
+#PreReqs: make, cmake, autoconf, libtools, kubectl nodejs>=14.8.0 & yarn 1.22.10 were working here
+conda install make cmake autoconf libtool nodejs yarn -y
+curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/ppc64le/kubectl"
+sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
+
 sudo env "PATH=$PATH" make argo-server.key
 sudo env "PATH=$PATH" make images
 
