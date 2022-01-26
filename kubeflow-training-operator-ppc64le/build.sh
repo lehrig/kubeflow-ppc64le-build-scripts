914caa8a078ee2c2a8b83dc52d3d35046924d274
Dr. Sebastian Lehrig <github@lehrig.de>
2022-01-26 23:54:25 +0100

    Update build.sh

--- kubeflow-training-operator-ppc64le/build.sh
+++ kubeflow-training-operator-ppc64le/build.sh
@@ -1,21 +1,23 @@
 #!/bin/sh
 
+# Needed for pip install
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+rustc --version
+
 # Follow https://github.com/kubeflow/training-operator/blob/master/docs/development/developer_guide.md
-
 export GIT_TRAINING=$(pwd)
-
 sudo env "PATH=$PATH" mkdir -p $(go env GOPATH)/src/github.com/kubeflow
 sudo env "PATH=$PATH" ln -sf ${GIT_TRAINING} $(go env GOPATH)/src/github.com/kubeflow/training-operator
 sudo env "PATH=$PATH" GO111MODULE="on" go mod vendor
 sudo env "PATH=$PATH" go install github.com/kubeflow/tf-operator/cmd/training-operator.v1
 
+# ppc64le fixes
 sed -i 's/GOARCH=amd64/GOARCH=ppc64le/g' build/images/training-operator/Dockerfile
 sed -i 's/from kubeflow.testing import util/#from kubeflow.testing import util/g' py/kubeflow/tf_operator/util.py
 sed -i 's/should_push=True/should_push=False/g' py/kubeflow/tf_operator/release.py
 
 cd py
-# https://techoverflow.net/2021/10/05/how-to-fix-pio-remote-agent-start-error-cant-find-rust-compiler/
-sudo apt -y install rustc
+
 # compiled this list of packages from error messages of the subsequent python command
 pip install --upgrade --quiet --no-cache-dir filelock pyyaml google-api-python-client google-cloud-speech google-cloud-storage jinja2 kubernetes
 sudo env "PATH=$PATH" env "GOPATH=$GOPATH" python -m kubeflow.tf_operator.release local
