c8bcf7d4875313824ff20aac7f58f48c76dcfa6b
Dr. Sebastian Lehrig <github@lehrig.de>
2022-02-25 09:33:10 +0100

    Update build.sh

--- kubeflow-notebook-image-ppc64le/build.sh
+++ kubeflow-notebook-image-ppc64le/build.sh
@@ -12,7 +12,7 @@ export TARGET=${REGISTRY}/${IMAGE}:${TAG}
 
 sudo chmod 777 /var/run/docker.sock
 
-docker build --build-arg NB_GID=0 --build-arg TENSORFLOW_VERSION=$TENSORFLOW_VERSION --build-arg PYTHON_VERSION=$PYTHON_VERSION --build-arg ELYRA_VERSION=$ELYRA_VERSION --build-arg TARGET_RUNTIME=$TARGET_RUNTIME -t $TARGET -f Dockerfile.all-in-one .
+docker build --squash --build-arg NB_GID=0 --build-arg TENSORFLOW_VERSION=$TENSORFLOW_VERSION --build-arg PYTHON_VERSION=$PYTHON_VERSION --build-arg ELYRA_VERSION=$ELYRA_VERSION --build-arg TARGET_RUNTIME=$TARGET_RUNTIME -t $TARGET -f Dockerfile.all-in-one .
 
 set +x
 echo $quay_p | sudo docker login --username $quay_u --password-stdin https://quay.io
