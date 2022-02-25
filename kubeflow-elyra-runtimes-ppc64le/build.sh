2dd47ae7c5cea8aa48188b9d329a3bc0c05bc2a1
Dr. Sebastian Lehrig <github@lehrig.de>
2022-02-25 09:25:51 +0100

    Update build.sh

--- kubeflow-elyra-runtimes-ppc64le/build.sh
+++ kubeflow-elyra-runtimes-ppc64le/build.sh
@@ -24,7 +24,7 @@ export IMAGE=$REGISTRY/${IMAGE}:${TAG}
 
 sudo chmod 777 /var/run/docker.sock
 
-docker build --build-arg TARGET_RUNTIME=$TARGET_RUNTIME --build-arg NB_GID=0 --build-arg elyra_version=$ELYRA_VERSION --build-arg PYTHON_VERSION=$PYTHON_VERSION --build-arg conda_version=$CUSTOM_CONDA_VERSION --build-arg miniforge_patch_number=$MINIFORGE_PATCH_NUMBER --build-arg PANDAS_VERSION=$PANDAS_VERSION --build-arg PYTORCH_VERSION=$PYTORCH_VERSION --build-arg R_VERSION=$R_VERSION --build-arg TENSORFLOW_VERSION=$TENSORFLOW_VERSION -t $IMAGE -f Dockerfile .
+docker build --squash --build-arg TARGET_RUNTIME=$TARGET_RUNTIME --build-arg NB_GID=0 --build-arg elyra_version=$ELYRA_VERSION --build-arg PYTHON_VERSION=$PYTHON_VERSION --build-arg conda_version=$CUSTOM_CONDA_VERSION --build-arg miniforge_patch_number=$MINIFORGE_PATCH_NUMBER --build-arg PANDAS_VERSION=$PANDAS_VERSION --build-arg PYTORCH_VERSION=$PYTORCH_VERSION --build-arg R_VERSION=$R_VERSION --build-arg TENSORFLOW_VERSION=$TENSORFLOW_VERSION -t $IMAGE -f Dockerfile .
 
 set +x
 echo $quay_p | sudo docker login --username $quay_u --password-stdin https://quay.io
