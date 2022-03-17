921350103ccff31d96af13a2aa19f4e22084ac65
Dr. Sebastian Lehrig <github@lehrig.de>
2022-03-17 22:26:56 +0100

    Update build.sh

--- kubeflow-katib-ppc64le/build.sh
+++ kubeflow-katib-ppc64le/build.sh
@@ -4,10 +4,14 @@ sudo apt-get update -y && sudo apt-get install openjdk-11-jdk -y
 
 sed -i 's/set -e//g' pkg/apis/manager/v1beta1/build.sh
 sed -i 's/znly\/protoc/docker.io\/mgiessing\/protoc/g' pkg/apis/manager/v1beta1/build.sh
+
 sed -i 's/set -e//g' pkg/apis/manager/health/build.sh
 sed -i 's/znly\/protoc/docker.io\/mgiessing\/protoc/g' pkg/apis/manager/health/build.sh
+
 sed -i 's/FROM pseudomuto\/protoc-gen-doc/FROM docker.io\/mgiessing\/protoc-gen-doc:1.5.7/g' pkg/apis/manager/v1beta1/gen-doc/Dockerfile
 sed -i 's/node:12/ppc64le\/node:12/g' cmd/new-ui/v1beta1/Dockerfile
+
+sed -i 's/set -e//g' scripts/v1beta1/build.sh
 sed -i '/Building Katib cert generator image/,$d' scripts/v1beta1/build.sh
 
 sudo env "PATH=$PATH" env "GOROOT=$GOROOT" env "GOPATH=$GOPATH" make build REGISTRY=quay.io/ibm TAG=${RELEASE} CPU_ARCH=ppc64le
