a8d0eb82a13b0ca839b9c10fe05ca9056303fbda
Dr. Sebastian Lehrig <github@lehrig.de>
2022-03-18 11:20:56 +0100

    Update build.sh

--- kubeflow-katib-ppc64le/build.sh
+++ kubeflow-katib-ppc64le/build.sh
@@ -1,6 +1,19 @@
 #!/bin/sh
+    
+cat >> expected-fail.txt <<'EOF'
+define EXPECTED_FAIL
+if ! { $1 ; } 2>$@.temp; then \
+    echo EXPECTED FAILURE: ; cat $@.temp; \
+fi
+endef
+EOF
 
-sudo apt-get update -y && sudo apt-get install openjdk-11-jdk -y
+sed -i "/# for pytest/r expected-fail.txt" Makefile
+
+sed -i 's/go generate .\/pkg\/... .\/cmd\/.../$(call EXPECTED_FAIL, go generate .\/pkg\/... .\/cmd\/...)/g' Makefile
+sed -i 's/hack\/gen-python-sdk\/gen-sdk.sh/$(call EXPECTED_FAIL, hack\/gen-python-sdk\/gen-sdk.sh)/g' Makefile
+sed -i 's/cd .\/pkg\/apis\/manager\/v1beta1 && .\/build.sh/$(call EXPECTED_FAIL, cd .\/pkg\/apis\/manager\/v1beta1 && .\/build.sh)/g' Makefile
+sed -i 's/cd .\/pkg\/apis\/manager\/health && .\/build.sh/$(call EXPECTED_FAIL, cd .\/pkg\/apis\/manager\/health && .\/build.sh)/g' Makefile
 
 sed -i 's/set -e//g' pkg/apis/manager/v1beta1/build.sh
 sed -i 's/znly\/protoc/docker.io\/mgiessing\/protoc/g' pkg/apis/manager/v1beta1/build.sh
@@ -15,6 +28,7 @@ sed -i 's/node:12/ppc64le\/node:12/g' cmd/new-ui/v1beta1/Dockerfile
 sed -i 's/set -e//g' scripts/v1beta1/build.sh
 sed -i '/Building Katib cert generator image/,$d' scripts/v1beta1/build.sh
 
+sudo apt-get update -y && sudo apt-get install openjdk-11-jdk -y
 sudo env "PATH=$PATH" env "GOROOT=$GOROOT" env "GOPATH=$GOPATH" make build REGISTRY=quay.io/ibm TAG=${RELEASE} CPU_ARCH=ppc64le
 
 set +x
