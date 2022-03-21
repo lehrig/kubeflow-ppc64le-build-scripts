45f2c86aa554563834d9f6cfdfc67328cec7b789
Dr. Sebastian M. Lehrig <lehrig@mac.fritz.box>
2022-03-21 14:39:10 +0100

    Added KServe

---
+++ kserve-models-web-app-ppc64le/build.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+sed -i 's/FROM node/FROM ppc64le\/node/g' Dockerfile
+
+sudo make build IMG=quay.io/ibm/${IMAGE} GIT_TAG=${RELEASE}
+
+set +x
+echo $quay_p | sudo docker login --username $quay_u --password-stdin https://quay.io
+set -x
+
+sudo docker push quay.io/ibm/${IMAGE}:${RELEASE}
