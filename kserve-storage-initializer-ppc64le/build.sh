e53c6f9e89699ced12ea95876093f577249adbf7
Dr. Sebastian Lehrig <github@lehrig.de>
2022-03-22 15:22:07 +0100

    Update build.sh

--- kserve-storage-initializer-ppc64le/build.sh
+++ kserve-storage-initializer-ppc64le/build.sh
@@ -5,17 +5,7 @@ RUN mkdir ~/.pip && \
     echo "[global]" >> ~/.pip/pip.conf && \
     echo "extra-index-url = https://repo.fury.io/mgiessing" >> ~/.pip/pip.conf
 EOF
-
 sed -i "/FROM python/r configure-gemfury.txt" python/storage-initializer.Dockerfile
-
-
-
-
-
-
-
-
-
 
 sudo env "PATH=$PATH" make docker-build-storageInitializer KO_DOCKER_REPO=quay.io/ibm STORAGE_INIT_IMG=${IMAGE}:${RELEASE}
 
