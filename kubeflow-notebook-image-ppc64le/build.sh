e5535067ec933f399c6f693bd173f75d7f3df78e
Dr. Sebastian Lehrig <github@lehrig.de>
2022-04-22 15:48:44 +0200

    Multi-stage builds

--- kubeflow-notebook-image-ppc64le/build.sh
+++ kubeflow-notebook-image-ppc64le/build.sh
@@ -1,18 +1,30 @@
 #!/bin/sh
 
 export REGISTRY=quay.io/ibm
-case "$TARGET_RUNTIME" in
-   "tensorflow") export RUNTIME_VERSION=$TENSORFLOW_VERSION
+export TARGET=${REGISTRY}/${IMAGE}:${TAG}
+export TAG=elyra${ELYRA_VERSION}-py${PYTHON_VERSION}
+case "$TARGET_DOCKER_FILE" in
+   "Dockerfile.base") export TAG=$TAG-base
    ;;
-   "tensorflow-cpu") export RUNTIME_VERSION=$TENSORFLOW_VERSION
+   "Dockerfile.minimal") export TAG=$TAG-min
+   ;;
+   "Dockerfile.scipy") export TAG=$TAG-scipy
+   ;;  
+   "Dockerfile.tensorflow-cpu")
+     export TAG=$TAG-tensorflow-cpu${TENSORFLOW_VERSION}
+     export TARGET_RUNTIME=tensorflow-cpu
+     
+   ;;
+   "Dockerfile.tensorflow-gpu")
+     export TAG=$TAG-tensorflow-gpu${TENSORFLOW_VERSION}
+     export TARGET_RUNTIME=tensorflow
    ;;
 esac
-export TAG=elyra${ELYRA_VERSION}-py${PYTHON_VERSION}-${TARGET_RUNTIME}${RUNTIME_VERSION}
-export TARGET=${REGISTRY}/${IMAGE}:${TAG}
+
 
 sudo chmod 777 /var/run/docker.sock
 
-docker build --squash --build-arg NB_GID=0 --build-arg TENSORFLOW_VERSION=$TENSORFLOW_VERSION --build-arg PYTHON_VERSION=$PYTHON_VERSION --build-arg ELYRA_VERSION=$ELYRA_VERSION --build-arg TARGET_RUNTIME=$TARGET_RUNTIME -t $TARGET -f Dockerfile.all-in-one .
+docker build --squash --build-arg NB_GID=0 --build-arg ELYRA_VERSION=$ELYRA_VERSION --build-arg PYTHON_VERSION=$PYTHON_VERSION --build-arg TENSORFLOW_VERSION=$TENSORFLOW_VERSION --build-arg TARGET_RUNTIME=$TARGET_RUNTIME -t $TARGET -f $TARGET_DOCKER_FILE .
 
 set +x
 echo $quay_p | sudo docker login --username $quay_u --password-stdin https://quay.io
