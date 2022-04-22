231203cd443b521ccd2613ed0b2c716cb6828b43
Dr. Sebastian Lehrig <github@lehrig.de>
2022-04-22 15:01:16 +0200

    Update build.sh

--- kubeflow-notebook-base-image-ppc64le/build.sh
+++ kubeflow-notebook-base-image-ppc64le/build.sh
@@ -2,10 +2,10 @@
 
 export REGISTRY=quay.io/ibm
 
-export TAG=base-py${PYTHON_VERSION}
+export TAG=base-elyra${ELYRA_VERSION}-py${PYTHON_VERSION}
 export TARGET=$REGISTRY/${IMAGE}:${TAG}
 
-sudo docker build --build-arg NB_GID=0 --build-arg PYTHON_VERSION=${PYTHON_VERSION} -t $TARGET -f Dockerfile.base .
+sudo docker build --build-arg NB_GID=0 --build-arg PYTHON_VERSION=${PYTHON_VERSION} --build-arg ELYRA_VERSION=$ELYRA_VERSION -t $TARGET -f Dockerfile.base .
 
 set +x
 echo $quay_p | sudo docker login --username $quay_u --password-stdin https://quay.io
