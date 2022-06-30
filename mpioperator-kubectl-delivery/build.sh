154f8c328762a64c00124c03ae864982e8f9a60e
lehrig <github@lehrig.de>
2022-06-30 21:44:35 +0200

    Fixed incorrect use of make - use docker

--- mpioperator-kubectl-delivery/build.sh
+++ mpioperator-kubectl-delivery/build.sh
@@ -4,10 +4,14 @@ cd cmd/kubectl-delivery
 
 sed -i 's/amd64/ppc64le/g' Dockerfile
 
-sudo make build IMG=quay.io/ibm/${IMAGE} TAG=${RELEASE}
+export TARGET=quay.io/ibm/${IMAGE}:${RELEASE}
+
+sudo chmod 777 /var/run/docker.sock
+
+sudo docker build --squash -t ${TARGET} .
 
 set +x
 echo $quay_p | sudo docker login --username $quay_u --password-stdin https://quay.io
 set -x
 
-sudo docker push quay.io/ibm/${IMAGE}:${RELEASE}
+sudo docker push ${TARGET}
