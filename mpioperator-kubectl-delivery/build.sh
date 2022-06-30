8f2a87d134c93d4d77ff6ae02e49993b9b93f8ec
lehrig <github@lehrig.de>
2022-06-30 21:51:22 +0200

    using root path

--- mpioperator-kubectl-delivery/build.sh
+++ mpioperator-kubectl-delivery/build.sh
@@ -1,14 +1,12 @@
 #!/bin/sh
 
-cd cmd/kubectl-delivery
-
-sed -i 's/amd64/ppc64le/g' Dockerfile
+sed -i 's/amd64/ppc64le/g' cmd/kubectl-delivery/Dockerfile
 
 export TARGET=quay.io/ibm/${IMAGE}:${RELEASE}
 
 sudo chmod 777 /var/run/docker.sock
 
-sudo docker build --squash -t ${TARGET} .
+sudo docker build --squash -t ${TARGET} -f cmd/kubectl-delivery/Dockerfile .
 
 set +x
 echo $quay_p | sudo docker login --username $quay_u --password-stdin https://quay.io
