8a199ab0d76a79f07740c3996fb93206f88ed117
Dr. Sebastian Lehrig <github@lehrig.de>
2022-07-20 16:55:32 +0200

    Update build.sh

--- kubeflow-notebook-image-ppc64le/build.sh
+++ kubeflow-notebook-image-ppc64le/build.sh
@@ -13,12 +13,14 @@ case "$TARGET_DOCKER_FILE" in
    ;;
    "Dockerfile.r") export TAG=$TAG-r
    ;;
+   "Dockerfile.pytorch") export TAG=$TAG-$(if [ "$SUPPORT_GPU" = true ]; then echo "pytorch-gpu"; else echo "pytorch-cpu"; fi)${PYTORCH_VERSION}
+   ;;
 esac
 export TARGET=${REGISTRY}/${IMAGE}:${TAG}
 
 sudo chmod 777 /var/run/docker.sock
 
-docker build --squash --build-arg NB_GID=0 --build-arg ELYRA_VERSION=$ELYRA_VERSION --build-arg PYTHON_VERSION=$PYTHON_VERSION --build-arg TENSORFLOW_VERSION=$TENSORFLOW_VERSION --build-arg SUPPORT_GPU=$SUPPORT_GPU -t $TARGET -f $TARGET_DOCKER_FILE .
+docker build --squash --build-arg NB_GID=0 --build-arg ELYRA_VERSION=$ELYRA_VERSION --build-arg PYTHON_VERSION=$PYTHON_VERSION --build-arg TENSORFLOW_VERSION=$TENSORFLOW_VERSION --build-arg PYTORCH_VERSION=$PYTORCH_VERSION --build-arg SUPPORT_GPU=$SUPPORT_GPU -t $TARGET -f $TARGET_DOCKER_FILE .
 
 set +x
 echo $quay_p | sudo docker login --username $quay_u --password-stdin https://quay.io
