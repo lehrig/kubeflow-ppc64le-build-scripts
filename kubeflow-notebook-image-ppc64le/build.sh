ca1e7ab878dbda79d95425ab109c342a35a97d91
Dr. Sebastian Lehrig <github@lehrig.de>
2022-07-28 23:39:35 +0200

    Update build.sh

--- kubeflow-notebook-image-ppc64le/build.sh
+++ kubeflow-notebook-image-ppc64le/build.sh
@@ -21,7 +21,7 @@ export TARGET=${REGISTRY}/${IMAGE}:${TAG}
 
 sudo chmod 777 /var/run/docker.sock
 
-docker build --squash --build-arg NB_GID=0 --build-arg ELYRA_VERSION=$ELYRA_VERSION --build-arg PYTHON_VERSION=$PYTHON_VERSION --build-arg TENSORFLOW_VERSION=$TENSORFLOW_VERSION --build-arg PYTORCH_VERSION=$PYTORCH_VERSION --build-arg SUPPORT_GPU=$SUPPORT_GPU -t $TARGET -f $TARGET_DOCKER_FILE .
+docker build --squash --build-arg NB_GID=0 --build-arg ELYRA_VERSION=$ELYRA_VERSION --build-arg PYTHON_VERSION=$PYTHON_VERSION --build-arg TENSORFLOW_VERSION=$TENSORFLOW_VERSION --build-arg PYTORCH_VERSION=$PYTORCH_VERSION --build-arg SUPPORT_GPU=$SUPPORT_GPU --build-arg BASE_MINOR_RELEASE=$BASE_MINOR_RELEASE -t $TARGET -f $TARGET_DOCKER_FILE .
 
 set +x
 echo $quay_p | sudo docker login --username $quay_u --password-stdin https://quay.io
