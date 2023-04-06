d42cb1dd05ef8e3cefdc2e71b27c42624694a635
Dr. Sebastian Lehrig <github@lehrig.de>
2023-04-06 13:24:18 +0200

    Update build.sh

--- copy-multi-arch-image/build.sh
+++ copy-multi-arch-image/build.sh
@@ -1,7 +1,3 @@
 #!/bin/sh
 
-set +x
-echo $quay_p | sudo docker login --username $quay_u --password-stdin https://quay.io
-set -x
-
-sudo docker run quay.io/skopeo/stable:latest copy --multi-arch all docker://$SOURCE_IMAGE docker://$TARGET_IMAGE
+sudo docker run quay.io/skopeo/stable:latest copy --dest-username $quay_u --dest-password $quay_p --multi-arch all docker://$SOURCE_IMAGE docker://$TARGET_IMAGE
