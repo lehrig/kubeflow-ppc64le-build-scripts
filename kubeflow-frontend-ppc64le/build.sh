c98a4ac45c940581198b83e1d62b916d653a8653
Dr. Sebastian Lehrig <github@lehrig.de>
2023-04-06 20:47:38 +0200

    Update build.sh

--- kubeflow-frontend-ppc64le/build.sh
+++ kubeflow-frontend-ppc64le/build.sh
@@ -2,7 +2,7 @@
 sed -i 's/FROM node:14\.18\.2 as build/FROM node:14\.18\.2-alpine as build/g' frontend/Dockerfile
 
 cd frontend
-sudo docker build --no-cache -t quay.io/ibm/${IMAGE}:${RELEASE} -f Dockerfile .
+sudo docker build -t quay.io/ibm/${IMAGE}:${RELEASE} -f Dockerfile .
 
 set +x
 echo $quay_p | sudo docker login --username $quay_u --password-stdin https://quay.io
