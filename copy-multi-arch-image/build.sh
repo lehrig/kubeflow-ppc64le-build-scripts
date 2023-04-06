d2bd4b2d6875e5aa128ab51ca626cac0cb8dd4c8
Dr. Sebastian Lehrig <github@lehrig.de>
2023-04-06 12:06:41 +0200

    Update build.sh

--- copy-multi-arch-image/build.sh
+++ copy-multi-arch-image/build.sh
@@ -1,9 +1,7 @@
 #!/bin/sh
 
-sudo apt-get update -y && sudo apt-get install skopeo -y
-
 set +x
 echo $quay_p | sudo docker login --username $quay_u --password-stdin https://quay.io
 set -x
 
-skopeo copy --multi-arch all docker://$SOURCE_IMAGE docker://$TARGET_IMAGE
+docker run docker://quay.io/skopeo/stable:latest copy --multi-arch all docker://$SOURCE_IMAGE docker://$TARGET_IMAGE
