ec7c73f58428197c4c61f8d244a0fd488d16439b
Dr. Sebastian Lehrig <github@lehrig.de>
2023-04-08 17:39:40 +0200

    Update build.sh

--- kubeflow-frontend-ppc64le/build.sh
+++ kubeflow-frontend-ppc64le/build.sh
@@ -2,8 +2,7 @@
 sed -i 's/FROM node:14\.18\.2 as build/FROM node:14\.18\.2-alpine as build/g' frontend/Dockerfile
 sed -i 's/RUN \.\/scripts\/yarn-licenses\.sh/#RUN \.\/scripts\/yarn-licenses\.sh/g' frontend/Dockerfile
 
-cd frontend
-sudo docker build -t quay.io/ibm/${IMAGE}:${RELEASE} -f Dockerfile .
+sudo docker build -t quay.io/ibm/${IMAGE}:${RELEASE} -f frontend/Dockerfile .
 
 set +x
 echo $quay_p | sudo docker login --username $quay_u --password-stdin https://quay.io
