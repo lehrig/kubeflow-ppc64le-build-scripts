a2609760cdf1ba1b8c990e93414dbbdaa9676c92
Dr. Sebastian Lehrig <github@lehrig.de>
2023-04-13 15:49:58 +0200

    Update build.sh

--- kserve-agent-ppc64le/build.sh
+++ kserve-agent-ppc64le/build.sh
@@ -1,11 +1,1 @@
-#!/bin/sh
-
-sed -i 's/amd64/ppc64le/g' agent.Dockerfile
-
-sudo env "PATH=$PATH" make docker-build-agent KO_DOCKER_REPO=quay.io/ibm AGENT_IMG=${IMAGE}:${RELEASE}
-
-set +x
-echo $quay_p | sudo docker login --username $quay_u --password-stdin https://quay.io
-set -x
-
-sudo docker push quay.io/ibm/${IMAGE}:${RELEASE}
+# Upstreamed
