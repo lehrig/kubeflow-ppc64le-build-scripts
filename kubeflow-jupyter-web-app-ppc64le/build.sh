92ae8169087e0086fff03a3bbe19f53e4703a726
Dr. Sebastian Lehrig <github@lehrig.de>
2023-04-13 15:48:08 +0200

    Update build.sh

--- kubeflow-jupyter-web-app-ppc64le/build.sh
+++ kubeflow-jupyter-web-app-ppc64le/build.sh
@@ -1,13 +1,1 @@
-#!/bin/sh
-
-cd components/crud-web-apps/jupyter
-
-sed -i 's/FROM node:12-buster-slim/FROM ppc64le\/node:12/g' Dockerfile
-
-sudo make docker-build IMG=quay.io/ibm/${IMAGE} TAG=${RELEASE}
-
-set +x
-echo $quay_p | sudo docker login --username $quay_u --password-stdin https://quay.io
-set -x
-
-sudo docker push quay.io/ibm/${IMAGE}:${RELEASE}
+# Upstreamed
