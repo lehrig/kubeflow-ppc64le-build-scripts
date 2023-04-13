d2a74ce705c005bc7a9b57c3f42087f0f3d97764
Dr. Sebastian Lehrig <github@lehrig.de>
2023-04-13 15:51:23 +0200

    Update build.sh

--- kserve-controller-ppc64le/build.sh
+++ kserve-controller-ppc64le/build.sh
@@ -1,9 +1,1 @@
-#!/bin/sh
-
-sudo docker build . -t quay.io/ibm/${IMAGE}:${RELEASE}
-
-set +x
-echo $quay_p | sudo docker login --username $quay_u --password-stdin https://quay.io
-set -x
-
-sudo docker push quay.io/ibm/${IMAGE}:${RELEASE}
+# Upstreamed
