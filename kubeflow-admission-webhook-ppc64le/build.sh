b970a93f7a12411a13058151755c443979972c56
Dr. Sebastian Lehrig <github@lehrig.de>
2023-04-13 15:45:56 +0200

    Update build.sh

--- kubeflow-admission-webhook-ppc64le/build.sh
+++ kubeflow-admission-webhook-ppc64le/build.sh
@@ -1,13 +1,1 @@
-#!/bin/sh
-
-cd components/admission-webhook
-
-sed -i 's/GOARCH=amd64/GOARCH=ppc64le/g' Dockerfile
-
-sudo make build-gcr IMG=quay.io/ibm/${IMAGE} TAG=${RELEASE}
-
-set +x
-echo $quay_p | sudo docker login --username $quay_u --password-stdin https://quay.io
-set -x
-
-sudo docker push quay.io/ibm/${IMAGE}:${RELEASE}
+# Upstreamed
