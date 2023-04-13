978979f6581da80604707519456f3f0f72df0513
Dr. Sebastian Lehrig <github@lehrig.de>
2023-04-13 15:57:33 +0200

    Update build.sh

--- kubeflow-notebook-controller-ppc64le/build.sh
+++ kubeflow-notebook-controller-ppc64le/build.sh
@@ -1,15 +1,1 @@
-#!/bin/sh
-
-cd components/notebook-controller
-
-sed -i 's/.\/third_party\/check-license.sh/#.\/third_party\/check-license.sh/g' Makefile
-sed -i 's/docker-build: test/docker-build: #test/g' Makefile
-sed -i 's/GOARCH=amd64/GOARCH=ppc64le/g' Dockerfile
-
-sudo env "PATH=$PATH" make docker-build IMG=quay.io/ibm/${IMAGE} TAG=${RELEASE}
-
-set +x
-echo $quay_p | sudo docker login --username $quay_u --password-stdin https://quay.io
-set -x
-
-sudo docker push quay.io/ibm/${IMAGE}:${RELEASE}
+# Upstreamed
