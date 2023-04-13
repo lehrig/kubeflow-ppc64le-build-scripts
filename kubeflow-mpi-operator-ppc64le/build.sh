6817f43ce9da25ce324c5b3c3389fc1474eaf411
Dr. Sebastian Lehrig <github@lehrig.de>
2023-04-13 15:56:46 +0200

    Update build.sh

--- kubeflow-mpi-operator-ppc64le/build.sh
+++ kubeflow-mpi-operator-ppc64le/build.sh
@@ -1,9 +1,1 @@
-#!/bin/sh
-
-sudo env "PATH=$PATH" make RELEASE_VERSION=${RELEASE} IMAGE_NAME=quay.io/ibm/${IMAGE} images
-
-set +x
-echo $quay_p | sudo docker login --username $quay_u --password-stdin https://quay.io
-set -x
-
-sudo docker push quay.io/ibm/${IMAGE}:${RELEASE}
+# Upstreamed
