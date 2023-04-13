b499aa074d205eb483f84774b1b7a03d52e3729c
Dr. Sebastian Lehrig <github@lehrig.de>
2023-04-13 15:58:42 +0200

    Update build.sh

--- kubeflow-profile-controller-ppc64le/build.sh
+++ kubeflow-profile-controller-ppc64le/build.sh
@@ -1,14 +1,1 @@
-#!/bin/sh
-
-cd components/profile-controller
-
-sed -i 's/GOARCH=amd64/GOARCH=ppc64le/g' Dockerfile
-
-sudo make build IMG=quay.io/ibm/${IMAGE} TAG=${RELEASE}
-
-
-set +x
-echo $quay_p | sudo docker login --username $quay_u --password-stdin https://quay.io
-set -x
-
-sudo docker push quay.io/ibm/${IMAGE}:${RELEASE}
+# Upstreamed
